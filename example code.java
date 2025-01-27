577da1a5d52735dd47adac48f4eb51e50785226b
Madhav Parmar <152685058+madhavparmar11@users.noreply.github.com>
2025-01-27 12:08:38 +0530

    Add files via upload

---
+++ example code.java
@@ -0,0 +1,16 @@
+class evenodd {
+    public static void main() {
+        for (int num = 1; num <= 10; num++) {
+            System.out.print(num);
+            if (num % 2 == 0)
+                System.out.println("even");
+            else
+                System.out.println("odd");
+        }
+    }
+
+    public static void main(String[] args) {
+        evenodd obj = new evenodd();
+        obj.main();
+    }
+}
